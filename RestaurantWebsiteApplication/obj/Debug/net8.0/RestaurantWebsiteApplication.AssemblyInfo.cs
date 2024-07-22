30fecafb8ad2f93aa833084968403e60e5fd7680
Manju Muggulla <17h41a0590@gmail.com>
2024-07-22 05:52:35 +0000

    add back image for home page

--- RestaurantWebsiteApplication/obj/Debug/net8.0/RestaurantWebsiteApplication.AssemblyInfo.cs
+++ RestaurantWebsiteApplication/obj/Debug/net8.0/RestaurantWebsiteApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RestaurantWebsiteApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1cb7ef4ed5d290c75f584f5d3a805329966d815")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e97275db02c0591e7a8dffc80f297978314ff9fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("RestaurantWebsiteApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RestaurantWebsiteApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
